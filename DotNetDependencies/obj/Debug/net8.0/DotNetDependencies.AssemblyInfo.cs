fdbcf4ad87fd3587880e9c613088d1bea448ef2a
Xavier <xavieralves162@gmail.com>
2026-03-17 15:56:59 +0000

    Added Humanize dependencie

--- DotNetDependencies/obj/Debug/net8.0/DotNetDependencies.AssemblyInfo.cs
+++ DotNetDependencies/obj/Debug/net8.0/DotNetDependencies.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotNetDependencies")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ffd587525788fe2d4f139f7fe557416ecff6f82")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotNetDependencies")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotNetDependencies")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
