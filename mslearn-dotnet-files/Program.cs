9c0812a7fad2ed4a87330ef643f0486986ef7200
Xavier <xavieralves162@gmail.com>
2026-03-18 16:12:43 +0000

    Web API backend

--- mslearn-dotnet-files/Program.cs
+++ mslearn-dotnet-files/Program.cs
@@ -1,16 +1,20 @@
 ﻿using System.IO;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 var currentDirectory = Directory.GetCurrentDirectory();
 
 var storesDirectory = Path.Combine(currentDirectory, "stores");
 
+var salesTotalDir = Path.Combine(currentDirectory, "salesTotalDir");
+
+Directory.CreateDirectory(salesTotalDir);
+
 var salesFiles = FindFiles(storesDirectory);
 
-foreach (var file in salesFiles)
-{
-    Console.WriteLine(file);
-}
+var salesTotal = CalculateSalesTotal(salesFiles);
+
+File.AppendAllText(Path.Combine(salesTotalDir, "totals.txt"), $"{salesTotal}{Environment.NewLine}");
 
 
 IEnumerable<string> FindFiles(string folderName)
@@ -30,3 +34,26 @@ IEnumerable<string> FindFiles(string folderName)
 
     return salesFiles;
 }
+
+double CalculateSalesTotal(IEnumerable<string> salesFiles)
+{
+    double salesTotal = 0;
+
+    // Loop over each file path in salesFiles
+    foreach (var file in salesFiles)
+    {      
+        // Read the contents of the file
+        string salesJson = File.ReadAllText(file);
+
+        // Parse the contents as JSON
+        SalesData? data = JsonConvert.DeserializeObject<SalesData?>(salesJson);
+
+        // Add the amount found in the Total field to the salesTotal variable
+        salesTotal += data?.Total ?? 0;
+    }
+
+    return salesTotal;
+}
+
+record SalesData (double Total);
+
